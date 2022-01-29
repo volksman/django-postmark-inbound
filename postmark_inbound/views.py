700eba6a30730a75b1cf714e838d261a39d63df6
skulegirl <anna@egate.net>
2022-01-28 19:03:15 -0500

    Fix ip_forward on webhook and bump version

--- postmark_inbound/views.py
+++ postmark_inbound/views.py
@@ -17,11 +17,9 @@ class PostmarkPermission(permissions.BasePermission):
     postmark_inbound_ip = option.IP_WHITE_LIST
 
     def has_permission(self, request, view):
-        print("Checking postmark IP")
         ip_forward = request.META.get('HTTP_X_FORWARDED_FOR')
-        print("ip_forward: ", ip_forward)
         if ip_forward:
-            remote_addr = ip_forward.split(",")[-1]
+            remote_addr = ip_forward.split(",")[0]
         else:
             remote_addr = (request.META.get('HTTP_X_REAL_IP') or
                         request.META.get('REMOTE_ADDR'))
