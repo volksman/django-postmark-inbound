c9cee4e8cd20bfead52140f21f76665904c6974b
volksman <v0lksman69@gmail.com>
2024-09-19 11:58:37 -0400

    remove provided args for DJ4 compat

--- postmark_inbound/signals.py
+++ postmark_inbound/signals.py
@@ -1,5 +1,4 @@
 import django.dispatch
 
-inbound_mail_received = django.dispatch.Signal(providing_args=["mail_data",
-                                                               "mail_object"])
-inbound_mail_filtered = django.dispatch.Signal(providing_args=["mail_data"])
+inbound_mail_received = django.dispatch.Signal("mail_data", "mail_object")
+inbound_mail_filtered = django.dispatch.Signal("mail_data")
